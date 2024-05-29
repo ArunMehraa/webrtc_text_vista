6510683b0bf930d3477ee6bba01e0aa603415760
me.Arun <93211897+ArunMehraa@users.noreply.github.com>
2024-05-29 19:17:03 +0530

    Update server.js

--- beckend/server.js
+++ beckend/server.js
@@ -56,8 +56,8 @@ const server = app.listen(PORT, () => {
 const io = require('socket.io')(server, {
     pingTimeout: 60000,
     cors: {
-        origin: 'http://localhost:3000',
-        // origin: 'https://small-talks-c376.onrender.com',
+        // origin: 'http://localhost:3000',
+        origin: 'https://textvista.netlify.app',
     },
 });
 
