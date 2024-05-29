fd4515aa64598633a7b98e8bbaabc6f92f065d23
me.Arun <93211897+ArunMehraa@users.noreply.github.com>
2024-05-29 20:10:36 +0530

    Update SingleChat.js

--- frontend/src/components/SingleChat.js
+++ frontend/src/components/SingleChat.js
@@ -23,7 +23,7 @@ import animationData from "../animations/typing.json";
 import { SocketContext } from "../Context/SocketContext";
 import { MdOutlineVideoCall } from "react-icons/md";
 
-const ENDPOINT = "https://textvista.netlify.app";
+const ENDPOINT = "https://medotchat.netlify.app";
 //////////////////change this to your server address
 
 var socket, selectedChatCompare;
