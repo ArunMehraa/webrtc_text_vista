610f91727af23df00e6c185c3641c5c5dcf849ee
me.Arun <93211897+ArunMehraa@users.noreply.github.com>
2024-05-29 19:15:44 +0530

    Update server.js

--- beckend/server.js
+++ beckend/server.js
@@ -31,16 +31,16 @@ app.use('/api/message', messageRoutes);
 
 //--------Deployment Code--------//
 
-// const __dirname1 = path.resolve();
-// if(NODE_ENV === 'production'){
-//     app.use(express.static(path.join(__dirname1, '/frontend/build')));
-//     app.get('*', (req, res) => {res.sendFile(path.resolve(__dirname1, 'frontend', 'build', 'index.html'))});
-// }
-// else{
-//     app.get('/', (req, res) => {
-//         res.send('API is running....');
-//     });
-// }
+const __dirname1 = path.resolve();
+if(NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname1, '/frontend/build')));
+    app.get('*', (req, res) => {res.sendFile(path.resolve(__dirname1, 'frontend', 'build', 'index.html'))});
+}
+else{
+    app.get('/', (req, res) => {
+        res.send('API is running....');
+    });
+}
 
 
 //--------Deployment Code--------//
